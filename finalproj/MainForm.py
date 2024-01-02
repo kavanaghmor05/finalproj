d018fa58cda6387a5b5b8d9b5934acdeaa1beb55
kavanaghmor05 <143837602+kavanaghmor05@users.noreply.github.com>
2024-01-02 13:41:58 -0600

    Update 1/2/24 1:40 pm
    This is an update made in class
    Things to finish tonight: Walking forward and inserting ShellPC images

--- finalproj/MainForm.py
+++ finalproj/MainForm.py
@@ -9,61 +9,97 @@ class MainForm(Form):
 		self.InitializeComponent()
 	
 	def InitializeComponent(self):
-		self._PCShellF = System.Windows.Forms.PictureBox()
-		self._AISpookyBOOF = System.Windows.Forms.PictureBox()
-		self._PCShellB = System.Windows.Forms.PictureBox()
-		self._AISpookyBOOB = System.Windows.Forms.PictureBox()
-		self._PCShellF.BeginInit()
-		self._AISpookyBOOF.BeginInit()
-		self._PCShellB.BeginInit()
-		self._AISpookyBOOB.BeginInit()
+		self._AISpookyBOO = System.Windows.Forms.PictureBox()
+		self._CompOn1 = System.Windows.Forms.PictureBox()
+		self._CompOn2 = System.Windows.Forms.PictureBox()
+		self._CompOn3 = System.Windows.Forms.PictureBox()
+		self._ShellPCStill = System.Windows.Forms.PictureBox()
+		self._ShellPcM1 = System.Windows.Forms.PictureBox()
+		self._ShellPcM2 = System.Windows.Forms.PictureBox()
+		self._AISpookyBOO.BeginInit()
+		self._CompOn1.BeginInit()
+		self._CompOn2.BeginInit()
+		self._CompOn3.BeginInit()
+		self._ShellPCStill.BeginInit()
+		self._ShellPcM1.BeginInit()
+		self._ShellPcM2.BeginInit()
 		self.SuspendLayout()
 		# 
-		# PCShellF
+		# AISpookyBOO
 		# 
-		self._PCShellF.Location = System.Drawing.Point(12, 12)
-		self._PCShellF.Name = "PCShellF"
-		self._PCShellF.Size = System.Drawing.Size(50, 75)
-		self._PCShellF.TabIndex = 0
-		self._PCShellF.TabStop = False
+		self._AISpookyBOO.Location = System.Drawing.Point(79, 12)
+		self._AISpookyBOO.Name = "AISpookyBOO"
+		self._AISpookyBOO.Size = System.Drawing.Size(92, 109)
+		self._AISpookyBOO.TabIndex = 1
+		self._AISpookyBOO.TabStop = False
 		# 
-		# AISpookyBOOF
+		# CompOn1
 		# 
-		self._AISpookyBOOF.Location = System.Drawing.Point(79, 12)
-		self._AISpookyBOOF.Name = "AISpookyBOOF"
-		self._AISpookyBOOF.Size = System.Drawing.Size(92, 109)
-		self._AISpookyBOOF.TabIndex = 1
-		self._AISpookyBOOF.TabStop = False
+		self._CompOn1.Location = System.Drawing.Point(177, 12)
+		self._CompOn1.Name = "CompOn1"
+		self._CompOn1.Size = System.Drawing.Size(37, 33)
+		self._CompOn1.TabIndex = 4
+		self._CompOn1.TabStop = False
 		# 
-		# PCShellB
+		# CompOn2
 		# 
-		self._PCShellB.Location = System.Drawing.Point(12, 93)
-		self._PCShellB.Name = "PCShellB"
-		self._PCShellB.Size = System.Drawing.Size(50, 75)
-		self._PCShellB.TabIndex = 2
-		self._PCShellB.TabStop = False
+		self._CompOn2.Location = System.Drawing.Point(220, 12)
+		self._CompOn2.Name = "CompOn2"
+		self._CompOn2.Size = System.Drawing.Size(37, 33)
+		self._CompOn2.TabIndex = 5
+		self._CompOn2.TabStop = False
 		# 
-		# AISpookyBOOB
+		# CompOn3
 		# 
-		self._AISpookyBOOB.Location = System.Drawing.Point(79, 127)
-		self._AISpookyBOOB.Name = "AISpookyBOOB"
-		self._AISpookyBOOB.Size = System.Drawing.Size(92, 109)
-		self._AISpookyBOOB.TabIndex = 3
-		self._AISpookyBOOB.TabStop = False
+		self._CompOn3.Location = System.Drawing.Point(263, 12)
+		self._CompOn3.Name = "CompOn3"
+		self._CompOn3.Size = System.Drawing.Size(37, 33)
+		self._CompOn3.TabIndex = 6
+		self._CompOn3.TabStop = False
+		# 
+		# ShellPCStill
+		# 
+		self._ShellPCStill.Location = System.Drawing.Point(12, 12)
+		self._ShellPCStill.Name = "ShellPCStill"
+		self._ShellPCStill.Size = System.Drawing.Size(61, 58)
+		self._ShellPCStill.TabIndex = 7
+		self._ShellPCStill.TabStop = False
+		# 
+		# ShellPcM1
+		# 
+		self._ShellPcM1.Location = System.Drawing.Point(12, 76)
+		self._ShellPcM1.Name = "ShellPcM1"
+		self._ShellPcM1.Size = System.Drawing.Size(61, 58)
+		self._ShellPcM1.TabIndex = 8
+		self._ShellPcM1.TabStop = False
+		# 
+		# ShellPcM2
+		# 
+		self._ShellPcM2.Location = System.Drawing.Point(12, 140)
+		self._ShellPcM2.Name = "ShellPcM2"
+		self._ShellPcM2.Size = System.Drawing.Size(61, 58)
+		self._ShellPcM2.TabIndex = 9
+		self._ShellPcM2.TabStop = False
 		# 
 		# MainForm
 		# 
 		self.BackColor = System.Drawing.Color.Gray
-		self.ClientSize = System.Drawing.Size(996, 608)
-		self.Controls.Add(self._AISpookyBOOB)
-		self.Controls.Add(self._PCShellB)
-		self.Controls.Add(self._AISpookyBOOF)
-		self.Controls.Add(self._PCShellF)
+		self.ClientSize = System.Drawing.Size(900, 750)
+		self.Controls.Add(self._ShellPcM2)
+		self.Controls.Add(self._ShellPcM1)
+		self.Controls.Add(self._ShellPCStill)
+		self.Controls.Add(self._CompOn3)
+		self.Controls.Add(self._CompOn2)
+		self.Controls.Add(self._CompOn1)
+		self.Controls.Add(self._AISpookyBOO)
 		self.Name = "MainForm"
 		self.Text = "finalproj"
-		self._PCShellF.EndInit()
-		self._AISpookyBOOF.EndInit()
-		self._PCShellB.EndInit()
-		self._AISpookyBOOB.EndInit()
+		self._AISpookyBOO.EndInit()
+		self._CompOn1.EndInit()
+		self._CompOn2.EndInit()
+		self._CompOn3.EndInit()
+		self._ShellPCStill.EndInit()
+		self._ShellPcM1.EndInit()
+		self._ShellPcM2.EndInit()
 		self.ResumeLayout(False)
 
