ce27bcbb006a1b8e234c89d94c9c6ca753c0faca
HillLucy <143837696+HillLucy@users.noreply.github.com>
2024-01-17 13:50:56 -0600

    Updtae

--- finalproj/MainForm.py
+++ finalproj/MainForm.py
@@ -22,7 +22,7 @@ class MainForm(Form):
 		self._timerwalk = System.Windows.Forms.Timer(self._components)
 		self._timergame = System.Windows.Forms.Timer(self._components)
 		self._groupBox1 = System.Windows.Forms.GroupBox()
-		self._movecontr = System.Windows.Forms.Label()
+		self._wow = System.Windows.Forms.Label()
 		self._label1 = System.Windows.Forms.Label()
 		self._label2 = System.Windows.Forms.Label()
 		self._timerques1 = System.Windows.Forms.Timer(self._components)
@@ -44,6 +44,8 @@ class MainForm(Form):
 		self._compex = System.Windows.Forms.Button()
 		self._timerques2 = System.Windows.Forms.Timer(self._components)
 		self._timerques3 = System.Windows.Forms.Timer(self._components)
+		self._label3 = System.Windows.Forms.Label()
+		self._Door = System.Windows.Forms.Button()
 		self._AISpookyBOO.BeginInit()
 		self._ShellPCStill.BeginInit()
 		self._ShellPCM1.BeginInit()
@@ -55,9 +57,9 @@ class MainForm(Form):
 		# 
 		self._AISpookyBOO.BackColor = System.Drawing.Color.Transparent
 		self._AISpookyBOO.BackgroundImage = resources.GetObject("AISpookyBOO.BackgroundImage")
-		self._AISpookyBOO.Location = System.Drawing.Point(400, 52)
+		self._AISpookyBOO.Location = System.Drawing.Point(401, 12)
 		self._AISpookyBOO.Name = "AISpookyBOO"
-		self._AISpookyBOO.Size = System.Drawing.Size(92, 109)
+		self._AISpookyBOO.Size = System.Drawing.Size(92, 111)
 		self._AISpookyBOO.TabIndex = 1
 		self._AISpookyBOO.TabStop = False
 		# 
@@ -88,11 +90,12 @@ class MainForm(Form):
 		self._ShellPCM2.BackColor = System.Drawing.Color.Transparent
 		self._ShellPCM2.BackgroundImage = resources.GetObject("ShellPCM2.BackgroundImage")
 		self._ShellPCM2.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch
-		self._ShellPCM2.Location = System.Drawing.Point(12, 513)
+		self._ShellPCM2.Location = System.Drawing.Point(63, 25)
 		self._ShellPCM2.Name = "ShellPCM2"
 		self._ShellPCM2.Size = System.Drawing.Size(61, 58)
 		self._ShellPCM2.TabIndex = 9
 		self._ShellPCM2.TabStop = False
+		self._ShellPCM2.Click += self.ShellPCM2Click
 		# 
 		# timermonst
 		# 
@@ -112,43 +115,46 @@ class MainForm(Form):
 		self._groupBox1.BackColor = System.Drawing.Color.Linen
 		self._groupBox1.Controls.Add(self._label2)
 		self._groupBox1.Controls.Add(self._label1)
-		self._groupBox1.Controls.Add(self._movecontr)
+		self._groupBox1.Controls.Add(self._wow)
 		self._groupBox1.Location = System.Drawing.Point(728, 12)
 		self._groupBox1.Name = "groupBox1"
-		self._groupBox1.Size = System.Drawing.Size(144, 109)
+		self._groupBox1.Size = System.Drawing.Size(144, 138)
 		self._groupBox1.TabIndex = 10
 		self._groupBox1.TabStop = False
 		self._groupBox1.Text = "Controls"
 		self._groupBox1.Enter += self.GroupBox1Enter
 		# 
-		# movecontr
+		# wow
 		# 
-		self._movecontr.Font = System.Drawing.Font("SimSun", 9.75, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, 0)
-		self._movecontr.Location = System.Drawing.Point(6, 13)
-		self._movecontr.Name = "movecontr"
-		self._movecontr.Size = System.Drawing.Size(128, 32)
-		self._movecontr.TabIndex = 0
-		self._movecontr.Text = "To move use W-A-S-D"
+		self._wow.Font = System.Drawing.Font("SimSun", 9.75, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, 0)
+		self._wow.Location = System.Drawing.Point(0, 13)
+		self._wow.Name = "wow"
+		self._wow.Size = System.Drawing.Size(128, 43)
+		self._wow.TabIndex = 0
+		self._wow.Text = "Ask Mr.Szelogowski about his AI!"
+		self._wow.Click += self.MovecontrClick
 		# 
 		# label1
 		# 
 		self._label1.Font = System.Drawing.Font("SimSun", 9.75, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, 0)
-		self._label1.Location = System.Drawing.Point(6, 51)
+		self._label1.Location = System.Drawing.Point(0, 51)
 		self._label1.Name = "label1"
-		self._label1.Size = System.Drawing.Size(128, 22)
+		self._label1.Size = System.Drawing.Size(144, 43)
 		self._label1.TabIndex = 1
-		self._label1.Text = "To interact click"
+		self._label1.Text = """To interact click
+Press escape to close the game!"""
 		self._label1.Click += self.Label1Click
 		# 
 		# label2
 		# 
 		self._label2.Font = System.Drawing.Font("Chiller", 18, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, 0)
-		self._label2.Location = System.Drawing.Point(6, 64)
+		self._label2.Location = System.Drawing.Point(22, 103)
 		self._label2.Name = "label2"
-		self._label2.Size = System.Drawing.Size(128, 32)
+		self._label2.Size = System.Drawing.Size(99, 32)
 		self._label2.TabIndex = 2
 		self._label2.Text = "Good Luck!"
 		self._label2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+		self._label2.Click += self.Label2Click
 		# 
 		# Count1
 		# 
@@ -317,12 +323,45 @@ class MainForm(Form):
 		self._compex.UseVisualStyleBackColor = False
 		self._compex.Click += self.CompexClick
 		# 
+		# timerques2
+		# 
+		self._timerques2.Tick += self.Timerques2Tick
+		# 
+		# timerques3
+		# 
+		self._timerques3.Tick += self.Timerques3Tick
+		# 
+		# label3
+		# 
+		self._label3.BackColor = System.Drawing.Color.Linen
+		self._label3.Location = System.Drawing.Point(12, 89)
+		self._label3.Name = "label3"
+		self._label3.Size = System.Drawing.Size(89, 109)
+		self._label3.TabIndex = 27
+		# 
+		# Door
+		# 
+		self._Door.BackColor = System.Drawing.Color.Peru
+		self._Door.Font = System.Drawing.Font("Comic Sans MS", 9.75, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, 0)
+		self._Door.Location = System.Drawing.Point(1, 605)
+		self._Door.Name = "Door"
+		self._Door.Size = System.Drawing.Size(26, 104)
+		self._Door.TabIndex = 28
+		self._Door.Text = """D
+o
+o
+r"""
+		self._Door.UseVisualStyleBackColor = False
+		self._Door.Click += self.DoorClick
+		# 
 		# MainForm
 		# 
 		self.BackColor = System.Drawing.Color.Gray
 		self.BackgroundImage = resources.GetObject("$this.BackgroundImage")
 		self.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch
 		self.ClientSize = System.Drawing.Size(884, 711)
+		self.Controls.Add(self._Door)
+		self.Controls.Add(self._label3)
 		self.Controls.Add(self._compex)
 		self.Controls.Add(self._button2)
 		self.Controls.Add(self._button1)
@@ -379,12 +418,15 @@ class MainForm(Form):
 		self._timerques2.Enabled = False
 		self._timerques3.Enabled = False
 		self._timermonst.Enabled = False
+		self._label3.Visible = False
 		Still = self._ShellPCStill
 		M1 = self._ShellPCM1
 		M2 = self._ShellPCM2
 		M1.Visible = False
-		M2.Visible = False
-		Still.Visible = True
+		M2.Visible = True
+		Still.Visible = False
+		
+	
 
 	def TimergameTick(self, sender, e):
 		if ShellPCStill.Right >= Count1.Left and ShellPCStill.Bottom >= Count1.Top and ShellPCStill.Top <= Count1.Bottom and ShellPCStill.Left <= Count1.Right:
@@ -463,7 +505,7 @@ class MainForm(Form):
 		self._compex.Visible = False
 
 	def Button1Click(self, sender, e):
-		if str(self._compans1.Text) == "1+1=" and str(self._compans2.Text) == "Pangea" and str(self._compans3.Text) == "Shell-Uh-Gaow-Skee":
+		if str(self._compans1.Text) == "1+1=" and str(self._compans2.Text) == "Pangea" and str(self._compans3.Text) == "Suh-Zell-Go-wisk-ee":
 			self._timerques1.Enabled = True
 			self._compback.Visible = False
 			self._compques1.Visible = False
@@ -558,5 +600,29 @@ class MainForm(Form):
 	
 
 	def TimermonstTick(self, sender, e):
-		self._AISpookyBOO.Top += self.AIup
-		self._AISpookyBOO.Left += 8 * self.AIup
+		pass
+
+	def MovecontrClick(self, sender, e):
+		pass
+
+	def Label2Click(self, sender, e):
+		pass
+
+	def ShellPCM2Click(self, sender, e):
+		self._label3.Visible = True
+		self._label3.Text = "Szelogowski:  The AI has taken form! The power of music was too strong! Shut it down with the questions!"
+
+	def Timerques3Tick(self, sender, e):
+		if self._timerques1.Enabled == True and self._timerques2.Enabled == True and self._timerques3.Enabled == True:
+			self._label3.Visible = True
+			self._AISpookyBOO.Visible = False
+			self._label3.Text = "Szelogowski:  You did it! Wait this is after hours, LEAVE! There is no prize!"
+
+	def DoorClick(self, sender, e):
+		Application.Exit()
+
+	def Timerques2Tick(self, sender, e):
+		if self._timerques1.Enabled == True and self._timerques2.Enabled == True:
+			self._label3.Visible = True
+			self._AISpookyBOO.Visible = True
+			self._label3.Text = "Szelogowski:  The AI escaped! QUICKLY!"
